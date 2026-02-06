3243b26a44b5e2cb235cffb9307f98553af643b9
RezSat <yehantest@gmail.com>
2026-02-06 09:50:47 +0530

    vault backup: 2026-02-06 09:50:46

---
+++ 2104 Object Oriented Programming/activites/RectangleArea.java
@@ -0,0 +1,13 @@
+public public class RectangleArea {
+    public static void main(String[] args) {
+        int length = 8;
+        int width = 5;
+
+        int area = length * width;
+
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
+ {
+    
+}
