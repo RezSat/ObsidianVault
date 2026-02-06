52c082c33cb96e1131422a361202076530f24cf3
RezSat <yehantest@gmail.com>
2026-02-06 09:45:21 +0530

    vault backup: 2026-02-06 09:45:21

---
+++ 2104 Object Oriented Programming/activites/StudentInfo.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String studentName = "Yehan";
+        double gpa = 3.8;
+        String registrationNumber = "23CDS0843";
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Registration Number: " + registrationNumber);
+    }
+}
