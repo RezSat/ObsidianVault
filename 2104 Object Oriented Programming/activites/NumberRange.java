b3d1c3e6c3ba9702046d0472a7a0a0a27c3124fb
RezSat <yehantest@gmail.com>
2026-02-06 09:56:21 +0530

    vault backup: 2026-02-06 09:56:21

---
+++ 2104 Object Oriented Programming/activites/NumberRange.java
@@ -0,0 +1,11 @@
+public class NumberRange {
+    public static void main(String[] args) {
+        int number = 14;
+
+        if (number >= 10 && number <= 25) {
+            System.out.println("The number is between 10 and 25.");
+        } else {
+            System.out.println("The number is not between 10 and 25.");
+        }
+    }
+}
